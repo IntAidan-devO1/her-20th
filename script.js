6e2df37c4c4204d4e7af7e57009aa50e215a7ed7
Aidan Francis <aidan.francis@my.fiveislands.uwi.edu>
2026-04-15 09:08:53 -0400

    Added images, another html and changed code

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function showSurprise() {
     document.getElementById("hiddenMessage").style.display = "block";
 }
+
+function showMessage(index) {
+    var messages = [
+        "You are the most beautiful person I know 💖",
+        "You make everything feel better just by being around you 🤎",
+        "I am blessed to have you in my life 💕"
+    ];
+
+    document.getElementById("clickMessage").innerText = messages[index];
+}
